3f1d6dfadff781ffee1940114badf54e2557abe6
uzuran <archloverlinux@gmail.com>
2022-05-03 16:55:56 +0200

    Add log in window with the login button.

--- functions.py
+++ functions.py
@@ -4,6 +4,7 @@ import bcrypt
 
 # Add main screen basic function with the parameters.
 def main_screen():
+    """This is the basic main_screen, with some parameters, labels, colors, buttons and screen main.loop() function"""
     global screen
     screen = Tk()
     screen.geometry("500x500")
@@ -21,38 +22,34 @@ def main_screen():
 
 
 def login():
-    print('You, are log in')
+    """This was login screen"""
+    global third_screen
+    third_screen = Toplevel(screen)
+    third_screen.title("Please login !")
+    third_screen.geometry("500x500")
+    Label(third_screen, text="Please login!", bg="#d1dffa", width="300", height="2", font=("Calibri", 13)).pack()
+    # Blank line.
+    Label(text="").pack()
 
+    global user_name_verify
+    global user_pass_verify
 
-# Register user and add all information inside text document.
-def register_users():
-    username_info = user_name.get()
-    password_info = password.get()
-
-    if username_info.isalnum():
-        Label(second_screen, text="You can not have a numbers in name! ", fg="red",
-              font=("Calibri", 11)).pack()
-    else:
-        # From str to bytes.
-        byte_pass = bytes(password_info, encoding='utf8')
-        # Generate salt.
-        salt = bcrypt.gensalt()
-        hashed = bcrypt.hashpw(byte_pass, salt)
-
-        file = open(username_info, "w")
-        file.write(username_info + "\n")
-        hashed_to_str = str(hashed)
-        file.write(hashed_to_str)
-        file.close()
-
-        user_name_entry.delete(0, END)
-        pass_entry.delete(0, END)
-
-        Label(second_screen, text='Registration success.', fg="green", font=("Calibri", 11)).pack()
+    user_name_verify = StringVar()
+    user_pass_verify = StringVar()
+    Label(third_screen, text="Username").pack()
+    user_name_entry1 = Entry(third_screen, textvariable=user_name_verify)
+    user_name_entry1.pack()
+    Label(third_screen, text="").pack()
+    Label(third_screen, text="Password").pack()
+    user_pass_entry2 = Entry(third_screen, textvariable=user_pass_verify)
+    user_pass_entry2.pack()
+    Label(third_screen, text="").pack()
+    Button(third_screen, text="Login", width=10, height=1).pack()
 
 
 # Add a new window for register users.
 def register():
+    """This was the second screen for register new users"""
     global second_screen
     """Add a new screen parameters at top of level main screen"""
     second_screen = Toplevel(screen)
@@ -80,3 +77,35 @@ def register():
     pass_entry = Entry(second_screen, textvariable=password, show="*")
     pass_entry.pack()
     Button(second_screen, text="Register", width=10, height=1, command=register_users).pack()
+
+
+# Register user and add all information inside text document.
+def register_users():
+    """Function for register new users, validate name without numbers,store all info in text file, hash password."""
+    username_info = user_name.get()
+    password_info = password.get()
+
+    if username_info.isalnum():
+        Label(second_screen, text="You can not have a numbers in name! ", fg="red",
+              font=("Calibri", 11)).pack()
+
+    elif password_info == "":
+        Label(second_screen, text="Password can not contain the blank line", fg="red",
+              font=("Calibri", 11)).pack()
+    else:
+        # From str to bytes.
+        byte_pass = bytes(password_info, encoding='utf8')
+        # Generate salt.
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(byte_pass, salt)
+        # Open file with the write mode, write the info of the name and hashed password
+        file = open(username_info, "w")
+        file.write(username_info + "\n")
+        hashed_to_str = str(hashed)
+        file.write(hashed_to_str)
+        file.close()
+
+        user_name_entry.delete(0, END)
+        pass_entry.delete(0, END)
+
+        Label(second_screen, text='Registration success.', fg="green", font=("Calibri", 11)).pack()
