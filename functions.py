49324a4ebb9e5fd20558797f891cc1c6693620c4
uzuran <archloverlinux@gmail.com>
2022-05-03 19:27:22 +0200

    Add hashlib library, add some logic function for right login.

--- functions.py
+++ functions.py
@@ -1,21 +1,32 @@
 from tkinter import *
-import bcrypt
+import hashlib
+import os
 
 
 # Add main screen basic function with the parameters.
 def main_screen():
-    """This is the basic main_screen, with some parameters, labels, colors, buttons and screen main.loop() function"""
+    """This is the basic main_screen, with some parameters, labels, colors, buttons and screen main.loop()
+    function"""
     global screen
     screen = Tk()
     screen.geometry("350x200")
     screen.title("Metal sheet calculator.")
-    Label(text="Please login!Or register.", bg="#d1dffa", width="300", height="2", font=("Calibri", 13)).pack()
+    screen.eval('tk::PlaceWindow . center')
+
+    # Main label.
+    Label(text="Please login!Or register.", bg="#d1dffa", width="300", height="2",
+          font=("Calibri", 13)).pack()
 
     # Blank line.
     Label(text="").pack()
+
+    # Login button.
     Button(text="Login", width="30", height="2", command=login).pack()
+
     # Blank line.
     Label(text="").pack()
+
+    # Register button.
     Button(text="Register", width="30", height="2", command=register).pack()
 
     screen.mainloop()
@@ -27,24 +38,68 @@ def login():
     third_screen = Toplevel(screen)
     third_screen.title("Please login !")
     third_screen.geometry("350x450")
-    Label(third_screen, text="Please login!", bg="#d1dffa", width="300", height="2", font=("Calibri", 13)).pack()
+
+    # Main label.
+    Label(third_screen, text="Please login!", bg="#d1dffa", width="300", height="2",
+          font=("Calibri", 13)).pack()
     # Blank line.
     Label(text="").pack()
 
     global user_name_verify
     global user_pass_verify
+    global user_name_entry1
+    global user_pass_entry2
 
     user_name_verify = StringVar()
     user_pass_verify = StringVar()
+
+    # Username label
     Label(third_screen, text="Username").pack()
+
+    # Username entry.
     user_name_entry1 = Entry(third_screen, textvariable=user_name_verify)
     user_name_entry1.pack()
+
+    # Blank line.
     Label(third_screen, text="").pack()
     Label(third_screen, text="Password").pack()
-    user_pass_entry2 = Entry(third_screen, textvariable=user_pass_verify)
+
+    # User pass entry.
+    user_pass_entry2 = Entry(third_screen, textvariable=user_pass_verify, show="*")
     user_pass_entry2.pack()
+
+    # Blank line.
     Label(third_screen, text="").pack()
-    Button(third_screen, text="Login", width=10, height=1).pack()
+
+    # Login button.
+    Button(third_screen, text="Login", width=10, height=1, command=login_verify).pack()
+
+
+def login_verify():
+    user_name_v = user_name_verify.get()
+    password_v = user_pass_verify.get()
+
+
+    user_name_entry1.delete(0, END)
+    user_pass_entry2.delete(0, END)
+
+    list_of_users = os.listdir()
+
+    password_v = hashlib.md5(str.encode(password_v)).hexdigest()
+
+    if user_name_v in list_of_users:
+        file1 = open(user_name_v, "r")
+        verify = file1.read().splitlines()
+
+        if password_v in verify:
+            Label(third_screen, text="Login success!", fg="green",
+                  font=("Calibri", 11)).pack()
+        else:
+            Label(third_screen, text="Password was wrong!", fg="red",
+                  font=("Calibri", 11)).pack()
+    else:
+        Label(third_screen, text="User not found !", fg="red",
+              font=("Calibri", 11)).pack()
 
 
 # Add a new window for register users.
@@ -56,6 +111,7 @@ def register():
     second_screen.geometry("350x450")
     second_screen.title("Metal sheet calculator.")
 
+    # Global
     global user_name
     global password
     global user_name_entry
@@ -65,18 +121,32 @@ def register():
     user_name = StringVar()
     password = StringVar()
 
+    # Main label.
     Label(second_screen, text="Please enter your name and password.", bg="#d1dffa", width="300", height="2",
           font=("Calibri", 13)).pack()
-    Label(second_screen, text="").pack()
-    Label(second_screen, text="Username").pack()
-    user_name_entry = Entry(second_screen, textvariable=user_name)
-    user_name_entry.pack()
+
     # Blank line.
     Label(second_screen, text="").pack()
+
+    # Username label.
+    Label(second_screen, text="Username").pack()
+
+    # User entry.
+    user_name_entry = Entry(second_screen, textvariable=user_name)
+    user_name_entry.pack()
+
+    # Blank line.
+    Label(second_screen, text="").pack()
+    # Password label.
     Label(second_screen, text="Password").pack()
+
+    # User password entry.
     pass_entry = Entry(second_screen, textvariable=password, show="*")
     pass_entry.pack()
+
+    # Blank line.
     Label(second_screen, text="").pack()
+    # Register button.
     Button(second_screen, text="Register", width=10, height=1, command=register_users).pack()
 
 
@@ -86,19 +156,17 @@ def register_users():
     username_info = user_name.get()
     password_info = password.get()
 
-    if username_info.isalnum():
+    # If username have some numbers label send it on screen.
+    if username_info.isdigit():
         Label(second_screen, text="You can not have a numbers in name! ", fg="red",
               font=("Calibri", 11)).pack()
 
+    # If password have only blank on field label say it.
     elif password_info == "":
         Label(second_screen, text="Password can not contain the blank line", fg="red",
               font=("Calibri", 11)).pack()
     else:
-        # From str to bytes.
-        byte_pass = bytes(password_info, encoding='utf8')
-        # Generate salt.
-        salt = bcrypt.gensalt()
-        hashed = bcrypt.hashpw(byte_pass, salt)
+        hashed = hashlib.md5(str.encode(password_info)).hexdigest()
         # Open file with the write mode, write the info of the name and hashed password
         file = open(username_info, "w")
         file.write(username_info + "\n")
