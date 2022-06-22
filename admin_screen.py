63b258a9a341a725362977fbc121145c4a2c9743
uzuran <archloverlinux@gmail.com>
2022-06-22 12:55:58 +0200

    Add sidebar to the admin screen.

--- admin_screen.py
+++ admin_screen.py
@@ -29,9 +29,25 @@ def admin_screen_window(user_name_v):
     check_user = Label(admin_screen, text=str(user_name_label) + " Is log in now.", bg="green")
     check_user.pack(anchor="w")
 
-    # Add notebok of a materials.
-    notebook = ttk.Notebook(admin_screen)
-    notebook.pack(anchor='center')
+    # <-- SCROLL BAR -->
+
+    # Create a canvas.
+    my_canvas = Canvas(admin_screen)
+    my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+    # Add scrollbar to the canvas.
+    scroll_bar = Scrollbar(admin_screen, orient=VERTICAL, command=my_canvas.yview)
+    scroll_bar.pack(side=RIGHT, fill=Y)
+    # Configure the Canvas.
+    my_canvas.config(yscrollcommand=scroll_bar.set)
+    my_canvas.bind("<Configure>", lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
+    # Make new frame.
+    second_frame = Frame(my_canvas)
+    # Add a new frame to the  window in canvas
+    my_canvas.create_window((0, 0), window=second_frame, anchor="nw")
+
+    # Add notebook of a materials to the second frame.
+    notebook = ttk.Notebook(second_frame)
+    notebook.pack()
 
     # Add frame options.
     frame_options = {"width": "500",
